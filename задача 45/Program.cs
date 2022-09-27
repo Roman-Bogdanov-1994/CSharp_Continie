d532997ba15659fd9f2afb8a0082a2cfca9ffe26
Roman <brv161@gmail.com>
2022-09-27 21:31:51 +0300

    7

---
+++ задача 45/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите длину массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arrFirst = new int[n];
+int[] arrResult = new int[n];
+
+for (int i = 0; i < arrFirst.Length; i++)
+    arrFirst[i] = new Random().Next(1, 10);
+
+Console.WriteLine("[" + string.Join(", ", arrFirst) + "]");
+
+for (int i = 0; i < arrFirst.Length; i++)
+    arrResult[i] = arrFirst[i];
+
+Console.WriteLine("[" + string.Join(", ", arrResult) + "]");
