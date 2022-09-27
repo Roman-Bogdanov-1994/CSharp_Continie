89a8d8e67e3dedc8715d3bc4acedb608dffa791e
Roman <brv161@gmail.com>
2022-09-27 21:08:31 +0300

    4

---
+++ Задача 40/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите длину сторон треугольника");
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a + b <= c || a + c <= b || b + c <= a)
+    Console.WriteLine("Это не треугольник");  
+else
+    Console.WriteLine("Это треугольник");  
---
+++ Задача 42/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число в десятичнос системе");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = "";
+while (n > 0)
+{
+    int x = n % 2;
+    result = Convert.ToString(x) + result;
+    n = n / 2;
+}
+Console.WriteLine(result);
+
