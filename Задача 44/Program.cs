ddeac921dbaefcf02abc355363901398341236ef
Roman <brv161@gmail.com>
2022-09-27 21:29:54 +0300

    5

---
+++ Задача 44/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Количество чисел = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+arr[0] = 0;
+arr[1] = 1;
+for (int i = 2; i < n; i++)
+    arr[i] = arr[i - 1] + arr[i - 2];
+Console.WriteLine(string.Join(", ", arr));
