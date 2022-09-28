7a403e9030c21347d1f9b7c468670d8e8899a58c
Roman <brv161@gmail.com>
2022-09-28 15:23:48 +0300

    9

---
+++ Задача 49/Program.cs
@@ -0,0 +1,44 @@
+﻿void Input(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11);
+    }
+}
+
+void Print(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + " \t");
+        Console.WriteLine();
+    }
+}
+void Release(int[,] matrix)
+{
+    for(int i = 1; i < matrix.GetLength(0); i += 2)
+    {
+        for(int j = 1; j < matrix.GetLength(1); j += 2)
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+        Console.WriteLine();
+    }
+    
+}
+
+
+
+
+Console.Write("Введите количество строк ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[n, m];
+
+Input(matrix);
+Print(matrix);
+Release(matrix);
+Print(matrix);
---
+++ Задача 51/Program.cs
@@ -0,0 +1,47 @@
+﻿void Input(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11);
+    }
+}
+
+void Print(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + " \t");
+        Console.WriteLine();
+    }
+}
+
+int count = 0;
+void Release(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            if (i == j)
+                count = count + matrix[i, j];
+        
+    }
+}
+
+
+Console.Write("Введите количество строк ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[n, m];
+
+
+Input(matrix);
+Print(matrix);
+Release(matrix);
+Console.WriteLine();
+Console.Write("Сумма чисел диагонали = ");
+Console.WriteLine(count);
